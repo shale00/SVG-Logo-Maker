4b9fd9ddfda6addae040facc41bc84741ff0280a
sth <sth0067@gmail.com>
2023-04-11 18:18:36 -0500

    added questions and notes for shapes, added index functions

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const questions = require('./lib/questions.js');
 
+
+// Function to create new SVG file
+
+
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then((response) => {
+            createLogo(response);
+            console.log('Questions answered successfully.');
+        })
+        .catch((err) => console.log(err));
+};
 
 // Function call to initialize app
 init();
